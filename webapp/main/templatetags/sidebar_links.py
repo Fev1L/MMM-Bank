6d9ab7de098d1234c5f5f5819cf8b7a3322a97b8
Fev1L <demchvlad17@gmail.com>
2026-03-02 21:12:19 +0100

    index Update
    added stats and cards in index.html

--- webapp/main/templatetags/sidebar_links.py
+++ webapp/main/templatetags/sidebar_links.py
@@ -5,32 +5,20 @@ register = template.Library();
 @register.simple_tag
 def get_links():
     return [{
-        'name': 'Home',
+        'name': 'Cards',
         'href': '/',
-        'icon': 'fa-house',
-    }, {
-        'name': 'Cars',
-        'href': '/cars',
-        'icon': 'fa-car',
-    }, {
-        'name': 'Contact',
-        'href': '/contact',
-        'icon': 'fa-paper-plane',
-    }, {
-        'name': 'About',
-        'href': '/about',
-        'icon': 'fa-address-card',
+        'icon': 'fa-credit-card',
     },{
-        'name': 'News',
-        'href': '/news/',
-        'icon': 'fa-newspaper',
-    },{
-        'name': 'Add news',
-        'href': '/news/create',
-        'icon': 'fa-plus',
-    },{
-        'name': 'Forum',
-        'href': '/forum',
-        'icon': 'fa-comment', #look for your icon here https://fontawesome.com/search?ic=free
+        'name': 'Credits',
+        'href': '/credits',
+        'icon': 'fa-chart-pie',
+    }, {
+        'name': 'Deposites',
+        'href': '/deposites',
+        'icon': 'fa-money-bill-1',
+    }, {
+        'name': 'More',
+        'href': '/more',
+        'icon': 'fa-ellipsis',
     }]
     
