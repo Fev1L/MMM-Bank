55e9da6c82614dd76eb353b5dd2f65033223f71e
Fev1L <demchvlad17@gmail.com>
2026-03-03 21:16:57 +0100

    htmls for new apps

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -22,5 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls), # default, part of django
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('credits/', include('credits.urls')), 
+    path('deposites/', include('deposites.urls')), 
+    path('more/', include('more.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
