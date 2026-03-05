dbc9c781ba9821b5d30ac4f794d5f1803d8ffd6e
dimachubuk15 <dima.chubuk2@gmail.com>
2026-03-05 23:23:23 +0100

    credits module start

--- webapp/credits/views.py
+++ webapp/credits/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'credits/index.html')
+
+def credits_list(request):
+    return HttpResponse("Credits module working")
