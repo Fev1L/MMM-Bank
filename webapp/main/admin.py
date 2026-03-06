4847c427a580975ed0de9e0971df48b6e7fb6fe3
Fev1L <demchvlad17@gmail.com>
2026-03-06 14:18:58 +0100

    completed transactions
    Done transactions, added another table in the database "category" (it is needed for transactions)

--- webapp/main/admin.py
+++ webapp/main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Account, Transaction
+from .models import Account, Transaction, Category
 
 admin.site.register(Account)
 
 admin.site.register(Transaction)
+
+admin.site.register(Category)
--- webapp/main/models.py
+++ webapp/main/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.exceptions import ValidationError
+from django.db import transaction
 
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,25 +11,48 @@ class Account(models.Model):
 
     def __str__(self):
         return f"{self.user.username} account"
-    
-class Transaction(models.Model):
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
     DEPOSIT = "deposit"
     WITHDRAW = "withdraw"
-
     TRANSACTION_TYPES = [
         (DEPOSIT, "Deposit"),
         (WITHDRAW, "Withdraw"),
     ]
 
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=12, decimal_places=2)
     type = models.CharField(max_length=20, choices=TRANSACTION_TYPES)
+
+    def __str__(self):
+        return self.name
+class Transaction(models.Model):
+    DEPOSIT = "deposit"
+    WITHDRAW = "withdraw"
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, default="Transaction")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    type = models.CharField(max_length=20, choices=Category.TRANSACTION_TYPES)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @transaction.atomic
+    def save(self, *args, **kwargs):
+        if self.pk:
+            raise ValidationError("Transactions cannot be edited")
+        if self.type == self.DEPOSIT:
+            self.account.balance += self.amount
+        elif self.type == self.WITHDRAW:
+            if self.account.balance < self.amount:
+                raise ValidationError("Not enough money")
+            self.account.balance -= self.amount
+
+        self.account.save()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.type} - {self.amount}"
-    
+
 @receiver(post_save, sender=User)
 def create_account(sender, instance, created, **kwargs):
     if created:
