99d22ac759f94f17e2e4959d2a23763da7146467
Fev1L <demchvlad17@gmail.com>
2026-03-26 09:22:33 +0100

    Casino Update

--- webapp/more/models.py
+++ webapp/more/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.core.exceptions import ValidationError
+from django.db import models, transaction
 
-# Create your models here.
+from main.models import Account, Category
+
+
+class Casino_Transaction(models.Model):
+    WIN = "win"
+    LOSE = "lose"
+    TRANSACTION_TYPES = [
+        (WIN, "Win"),
+        (LOSE, "Lose"),
+    ]
+
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, default="Transaction")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.category.type} - {self.amount}"
--- webapp/more/views.py
+++ webapp/more/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 
+from .models import Casino_Transaction
+
 def index(request):
     return render(request, 'more/index.html')
 
 def casino_home(request):
     account = request.user.account
     balance = account.balance
-    return render(request, 'more/casino/casino.html',
-                  {'balance': balance}
-                  )
+    transactions = Casino_Transaction.objects.filter(account=account).order_by('-created_at')[:3]
+    temp = {
+        'balance': balance,
+        'transactions': transactions
+        }
+    return render(request, 'more/casino/casino.html', temp)
 
 def casino_slots(request):
     return render(request, 'more/casino/slots.html')
