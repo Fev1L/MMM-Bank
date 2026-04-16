ba96edd2fe23ee891b8133c8685525fc48bfc388
Fev1L <demchvlad17@gmail.com>
2026-04-16 14:57:58 +0200

    Angular bug fix

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,10 @@
-import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  PLATFORM_ID,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,16 +14,28 @@ import {authInterceptor} from './core/services/auth.interceptor';
 import {provideFirebaseApp, initializeApp, getApps} from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import {isPlatformBrowser} from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
 
     provideFirebaseApp(() => {
-      const existingApps = getApps();
-      return existingApps.length ? existingApps[0] : initializeApp(environment.firebase);
+      const platformId = inject(PLATFORM_ID);
+      if (isPlatformBrowser(platformId)) {
+        const apps = getApps();
+        return apps.length ? apps[0] : initializeApp(environment.firebase);
+      }
+      return {} as any;
     }),
-    provideFirestore(() => getFirestore()),
+
+    provideFirestore(() => {
+      const platformId = inject(PLATFORM_ID);
+      if (isPlatformBrowser(platformId)) {
+        return getFirestore();
+      }
+      return {} as any;
+    }),
 
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(
